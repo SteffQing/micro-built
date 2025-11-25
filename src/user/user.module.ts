875dff44f43661c6a2f606786eefb1eaf74e5bce
Steve Tomi <steveola23@gmail.com>
2025-11-25 21:11:26 +0100

    add missing module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserService } from './user.service';
 import { LoanModule } from './loan/loan.module';
 import { RepaymentsModule } from './repayments/repayments.module';
 import { DatabaseModule } from 'src/database/database.module';
+import { PPIService } from './ppi.service';
 
 @Module({
   imports: [DatabaseModule, LoanModule, RepaymentsModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PPIService],
   exports: [UserService],
 })
 export class UserModule {}
