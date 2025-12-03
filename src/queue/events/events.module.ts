0d872251537c6e8c4e188ecd4d62e88f24fee72e
Steve Tomi <steveola23@gmail.com>
2025-12-03 10:30:27 +0100

    fix module in events

--- src/queue/events/events.module.ts
+++ src/queue/events/events.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AuthService, UserService } from './events.service';
+import { AuthService, UserService, CustomerService } from './events.service';
 import { NotificationModule } from 'src/notifications/notifications.module';
 import { DatabaseModule } from 'src/database/database.module';
 import { AdminService } from './events.admin';
@@ -17,6 +17,6 @@ import { LoanModule as AdminLoanModule } from 'src/admin/loan/loan.module';
     UserLoanModule,
     AdminLoanModule,
   ],
-  providers: [AuthService, UserService, AdminService],
+  providers: [AuthService, UserService, AdminService, CustomerService],
 })
 export class EventsModule {}
